074b76db93ab3486f3cc0912b2abe1f13f42df03
skylar-deepmind <skylar142857@gmail.com>
2026-07-18 12:23:14 +0800

    fix: polish locale label display in LanguageSwitcher component

--- components/LanguageSwitcher.tsx
+++ components/LanguageSwitcher.tsx
@@ -17,7 +17,7 @@ export function LanguageSwitcher({ currentLocale }: { currentLocale: Locale }) {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129" />
         </svg>
-        {localeLabels[currentLocale]}
+        {/* {localeLabels[currentLocale]} */}
       </label>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-1 z-50 p-1 shadow bg-base-100 rounded-box w-32">
         {locales.map((loc) => (
